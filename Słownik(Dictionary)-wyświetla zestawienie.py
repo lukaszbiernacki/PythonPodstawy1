1f548dd51ca785b60e345d5951e419e4a0cdf5f5
Łukasz Biernacki <37264460+lukaszbiernacki@users.noreply.github.com>
2018-04-11 11:52:46 +0200

    Add files via upload

---
+++ Słownik(Dictionary)-wyświetla zestawienie.py
@@ -0,0 +1,6 @@
+#Słownik(Dictionary)-wyświetla zestawienie
+
+dictionary={'A':'80%-10%','B':'60%-80%','C':'50%-60%','D':'less then 50%'}
+for word in dictionary.keys():
+    print(word,'-',dictionary[word])
+
