b5176d79dae1bdadfbc8ddc3b43867dfca8f309a
Łukasz Biernacki <37264460+lukaszbiernacki@users.noreply.github.com>
2018-04-11 16:45:51 +0200

    Add files via upload

---
+++ matematyka(w.bezwzgledna_zaokraglenia_itp).py
@@ -0,0 +1,26 @@
+f_smaller = 3.141592653589793
+f_bigger = 3.87756392764
+
+print(f_smaller, f_bigger)
+print('\n')
+
+print(int(f_smaller),int(f_bigger))
+print('\n')
+
+print(abs(f_smaller),abs(-f_smaller))   #wartość bezwzględa
+print('\n')
+
+print(round(f_smaller,2),round(f_bigger,2),round(f_bigger,3)) #zaokrąglenie
+print('\n')
+
+print(min(f_smaller,f_bigger), max(f_smaller,f_bigger))
+print('\n')
+
+list=[1,2,3,4,5,4,4,5,4]
+
+print(sum(list), len(list))
+print('\n')
+print(sum(list)/len(list))
+print('\n')
+
+print(round(2.675,2)) #zauważ że zaokrągla do piątki, typ float tak ma
