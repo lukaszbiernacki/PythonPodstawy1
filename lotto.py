1aba99f5b2f34bbf7db65d43f2c593eb79d7f74e
Łukasz Biernacki <37264460+lukaszbiernacki@users.noreply.github.com>
2018-04-18 15:28:11 +0200

    Add files via upload

---
+++ lotto.py
@@ -0,0 +1,12 @@
+import random
+
+myNumbers = []
+
+while len(myNumbers) < 7 :
+    newNumber = random.randint(1, 49)
+    if newNumber in myNumbers:
+        print("Eliminated: ",newNumber)
+        continue
+    myNumbers.append(newNumber)
+
+print("Those numbers will win:",myNumbers)
